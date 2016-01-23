c546956cae047fdddca293ef1669c264fa294cd3
Roma <romanlychak@ukr.net>
2016-01-23 17:13:04 +0200

    refactor to java 8

--- src/main/java/rl/testTaskAkka/actors/AdderManager.java
+++ src/main/java/rl/testTaskAkka/actors/AdderManager.java
@@ -12,8 +12,6 @@ import akka.util.Timeout;
 import scala.concurrent.Future;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
